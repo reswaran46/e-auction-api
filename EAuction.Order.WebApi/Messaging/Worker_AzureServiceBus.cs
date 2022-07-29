8993507fc5cef1520ae34b7c4ea7eb2ccc5ea5ac
unknown <cogdotnet770@iiht.tech>
2022-07-29 17:25:14 +0530

    initial version

---
+++ EAuction.Order.WebApi/Messaging/Worker_AzureServiceBus.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EAuction.Bid.WebApi.Messaging
+{
+    public class Worker_AzureServiceBus 
+    {
+
+    }
+}
---
+++ EAuction.Products.Api/Repositories/Abstractions/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EAuction.Products.Api.Entities;
+
+namespace EAuction.Products.Api.Repositories.Abstractions
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProduct(string id);
+        Task Create(Product product);
+        Task<bool> Delete(string id);
+    }
+}
